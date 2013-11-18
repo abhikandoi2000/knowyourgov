35bf90ecc8fae4bc8532019bc18d897d4bd25018
Ashwini Khare <khare.ashwini@gmail.com>
2013-11-18 18:34:17 +0530

    Added auxiliary pages, script to detect location

--- knowyourgov/routes/__init__.py
+++ knowyourgov/routes/__init__.py
@@ -6,9 +6,32 @@ from knowyourgov.scripts import insert_politicians_in_db
 # import errors
 
 # landing page
+"""Home page
+"""
 @app.route('/')
 def homepage():
   return render_template('home.html')
+
+"""Search -> Politician Page
+"""
+@app.route('/search', methods= ['POST', 'GET'] )
+def search():
+ # query = request.form['q']
+  query = request.args.get('q')
+  return render_template('politican.html', q = query)
+
+
+""" 404 - Page
+"""
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
+""" 500 - Page
+"""
+@app.errorhandler(500)
+def page_not_found(error):
+	return render_template('500.html'), 500
 
 """JSON response containing information for a particular politician
 """
---
+++ knowyourgov/static/js/home.js
@@ -0,0 +1,78 @@
+// // AIzaSyCt302Eg6wtngduThQ9jEmcGQfHmn1XNQg
+
+// var initialLocation;
+// var siberia = new google.maps.LatLng(60, 105);
+// var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+// var browserSupportFlag =  new Boolean();
+
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 8,
+//     center: new google.maps.LatLng(-34.397, 150.644),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//     // region: 'IN'
+//   };
+
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+
+//   // Try W3C Geolocation (Preferred)
+//   if(navigator.geolocation) {
+//     browserSupportFlag = true;
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//       map.setCenter(initialLocation);
+//     }, function() {
+//       handleNoGeolocation(browserSupportFlag);
+//     });
+//   }
+//   // Browser doesn't support Geolocation
+//   else {
+//     browserSupportFlag = false;
+//     handleNoGeolocation(browserSupportFlag);
+//   }
+
+//   function handleNoGeolocation(errorFlag) {
+//     if (errorFlag == true) {
+//       alert("Geolocation service failed.");
+//       initialLocation = newyork;
+//     } else {
+//       alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+//       initialLocation = siberia;
+//     }
+//     map.setCenter(initialLocation);
+//   }
+	
+// }
+
+// // google.maps.visualRefresh = true;
+
+// // function loadScript() {
+// //   var script = document.createElement('script');
+// //   script.type = 'text/javascript';
+// //   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' +
+// //       'callback=initialize';
+// //   document.body.appendChild(script);
+// // }
+
+// window.onload = initialize;
+
+if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude,
+          lng = position.coords.longitude;
+      // map.setCenter(initialLocation);
+
+       $.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=false', function(response){
+          console.log(response);
+          $('#map-canvas').html('Current Location : ' +  response['results'][0]['formatted_address']);
+        })
+    }, function() {
+        
+
+
+    });
+
+
+  }
