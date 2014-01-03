d9e861c6374e11ef39f952ddb5e1be307e28507e
jayant1992 <jayant_jain1992@yahoo.co.in>
2014-01-03 19:44:53 +0530

    Reworks percentile finction to make it faster too

--- knowyourgov/scripts/stats/__init__.py
+++ knowyourgov/scripts/stats/__init__.py
@@ -5,25 +5,6 @@ import logging
   
 fields = ['attendance', 'debates', 'questions', 'bills']
 wealth_fields = ['net_worth', 'cash', 'property', 'other']
-# def get_averages(pol, fields, filters):
-# 	pols = Politician.all()
-# 	pols.filter('position =', pol.position)
-# 	for filter_ in filters:
-# 		pols.filter(filter_['property'], filter_['value'])
-# 	averages = {}
-# 	for field in fields:
-# 		total = 0
-# 		count = 0
-# 		for pol in pols:
-# 			total += getattr(pol, field)
-# 			count += 1
-# 		if not count:
-# 			averages[field] = 0
-# 		else:
-# 			averages[field] = float(total)/count
-# 			averages[field] = ceil(100 * averages[field])/100
-# 		logging.info(averages)
-# 	return averages
 
 def get_averages(pol, fields, filters):
 	pols = Politician.all()
@@ -45,7 +26,6 @@ def get_averages(pol, fields, filters):
 		else:
 			averages[field] = float(total[field])/count
 			averages[field] = ceil(100 * averages[field])/100
-		logging.info(averages)
 	return averages
 
 def get_percentiles(pol, fields, filters):
@@ -54,20 +34,23 @@ def get_percentiles(pol, fields, filters):
 	for filter_ in filters:
 		pols.filter(filter_['property'], filter_['value'])
 	percentile = {}
-	for field in fields:
-		greater = 0.0
-		total = 0.0
-		pol_value = getattr(pol, field)
-		for politician in pols:
+	greater = {}
+	total = 0
+	for politician in pols:
+		total += 1 
+		for field in fields:
+			if not(field in greater.keys()):
+				greater[field] = 0
+			pol_value = getattr(pol, field)
 			if politician.name != pol.name:
 				value = getattr(politician, field)
-				total += 1 
 				if pol_value > value:
-					greater += 1
+					greater[field] += 1
+	for field in fields:
 		if not total:
 			percentile[field] = 0
 		else:
-			percentile[field] = 100 * float(greater)/float(total)
+			percentile[field] = 100 * float(greater[field])/float(total)
 			percentile[field] = ceil(100 * percentile[field])/100
 	return percentile
 
