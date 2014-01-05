bf14632987810dcbfdc5a9108db80504b88fd0f6
jayant1992 <jayant_jain1992@yahoo.co.in>
2014-01-05 16:16:07 +0530

    1. Minor changes to party landing page 2. capwords added to party names

--- knowyourgov/__init__.py
+++ knowyourgov/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, url_for, render_template, request, make_response
+from string import capwords
 
 import models, config
 
 app = Flask('knowyourgov')
 app.config.from_object(config)
+app.jinja_env.filters['capwords'] = capwords
 
 from knowyourgov import routes
